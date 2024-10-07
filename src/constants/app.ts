99494ed0f13cb9cb76ee7f5bed09b8c47c95b38c
ibabkov <i.babkov.dev@gmail.com>
2024-10-07 18:44:55 -0400

    chore: update dependencies

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,7 +1,7 @@
-import { TLanguage } from '../types/language';
+import { Language } from '../types/language';
 
 export const DISPLAY_APP_TITLE = 'Perlin Noise Shader Animation';
 export const APP_TITLE = 'perlin-noise-animation';
 export const APP_DESCRIPTION = "An immersive animated shader-based background. It's inspired by classic Perlin Noise by Stefan Gustavson.";
 
-export const LANGUAGE: TLanguage = 'en';
+export const LANGUAGE: Language = 'en';
