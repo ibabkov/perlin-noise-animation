9a70c4385f7089295a7c1dd19c84c2a1f2165714
ibabkov <i.babkov.dev@gmail.com>
2024-10-15 00:43:21 -0400

    fix: adjust cursor position on resize

--- src/hooks/useCoordinates.ts
+++ src/hooks/useCoordinates.ts
@@ -7,6 +7,7 @@ export const useCoordinates = (ref: React.RefObject<HTMLCanvasElement>) => {
 		type: '2f',
 		value: [-1, -1],
 	});
+	const windowRef = useRef<[number, number]>([0, 0]);
 
 	useEffect(() => {
 		const handleMouseMove = (event: MouseEvent) => {
@@ -16,6 +17,7 @@ export const useCoordinates = (ref: React.RefObject<HTMLCanvasElement>) => {
 			const normalizedX = (event.clientX - rect.left) / rect.width;
 			const normalizedY = (event.clientY - rect.top) / rect.height;
 
+			windowRef.current = [rect.width, rect.height];
 			uniformRef.current.value = [normalizedX, 1 - normalizedY];
 		};
 
@@ -26,17 +28,31 @@ export const useCoordinates = (ref: React.RefObject<HTMLCanvasElement>) => {
 			const normalizedX = (touch.clientX - rect.left) / rect.width;
 			const normalizedY = (touch.clientY - rect.top) / rect.height;
 
+			windowRef.current = [rect.width, rect.height];
 			uniformRef.current.value = [normalizedX, 1 - normalizedY];
+		};
+
+		const handleResize = () => {
+			if (!ref.current) return;
+			const rect = ref.current.getBoundingClientRect();
+
+			uniformRef.current.value = [
+				(uniformRef.current.value[0] * windowRef.current[0]) / rect.width,
+				(uniformRef.current.value[1] * windowRef.current[1]) / rect.height,
+			];
+			windowRef.current = [rect.width, rect.height];
 		};
 
 		const element = ref.current;
 		if (element) {
 			element.addEventListener('mousemove', handleMouseMove);
 			element.addEventListener('touchmove', handleTouchMove);
+			window.addEventListener('resize', handleResize);
 
 			return () => {
 				element.removeEventListener('mousemove', handleMouseMove);
 				element.removeEventListener('touchmove', handleTouchMove);
+				window.removeEventListener('resize', handleResize);
 			};
 		}
 	}, [ref]);
